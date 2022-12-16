8757e74177c8cfe650fd19abc0496152f8bb292d
Kuper-S <kupkup1992@gmail.com>
2022-12-16 09:33:19 +0200

    add theme and home page .

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
+import { useEffect } from "react";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
+import Home from "./scenes/Home";
+
+// Scroll to top on route change
+const ScrollToTop = () => {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+}
+
+
 function App() {
   return (
-    <div className="App">
-     
+    <div className="app">
+      <BrowserRouter>
+        <ScrollToTop />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          
+          
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
