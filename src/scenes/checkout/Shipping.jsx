d9659192ea1bea868a75b00448d65b35b7fd2a73
Kuper-S <kupkup1992@gmail.com>
2022-12-20 12:10:37 +0200

    finish strapi .

--- src/scenes/checkout/Shipping.jsx
+++ src/scenes/checkout/Shipping.jsx
@@ -1,19 +1,19 @@
 import { Box, Checkbox, FormControlLabel, Typography } from "@mui/material";
 import AddressForm from "./AddressForm";
 
-function Shipping({
-    values,
-    touched,
-    errors,
-    handleChange,
-    handleBlur,
-    setFieldValue,
-  }) {
+const Shipping = ({
+  values,
+  touched,
+  errors,
+  handleChange,
+  handleBlur,
+  setFieldValue,
+}) => {
   return (
     <Box m="30px auto">
       {/* BILLING FORM */}
       <Box>
-      <Typography sx={{ mb: "15px" }} fontSize="18px">
+        <Typography sx={{ mb: "15px" }} fontSize="18px">
           Billing Information
         </Typography>
         <AddressForm
@@ -25,8 +25,43 @@ function Shipping({
           handleChange={handleChange}
         />
       </Box>
-    </Box>
-  )
-}
 
-export default Shipping
+      <Box mb="20px">
+        <FormControlLabel
+          control={
+            <Checkbox
+              defaultChecked
+              value={values.shippingAddress.isSameAddress}
+              onChange={() =>
+                setFieldValue(
+                  "shippingAddress.isSameAddress",
+                  !values.shippingAddress.isSameAddress
+                )
+              }
+            />
+          }
+          label="Same for Shipping Address"
+        />
+      </Box>
+
+      {/* SHIPPING FORM */}
+      {!values.shippingAddress.isSameAddress && (
+        <Box>
+          <Typography sx={{ mb: "15px" }} fontSize="18px">
+            Shipping Information
+          </Typography>
+          <AddressForm
+            type="shippingAddress"
+            values={values.shippingAddress}
+            touched={touched}
+            errors={errors}
+            handleBlur={handleBlur}
+            handleChange={handleChange}
+          />
+        </Box>
+      )}
+    </Box>
+  );
+};
+
+export default Shipping;
